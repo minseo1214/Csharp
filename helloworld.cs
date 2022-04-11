657a406c1ce5225bfe977af91b353224925c1782
minseo1214 <68543486+minseo1214@users.noreply.github.com>
2022-04-11 10:49:05 +0900

    Create helloworld.cs

---
+++ helloworld.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Helloword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.WriteLine(args.Length);
+            Console.ReadKey();
+        }
+    }
+}
